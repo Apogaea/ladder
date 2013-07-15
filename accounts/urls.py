ff1d947487da33afa7081e8fbabd68ef14de0481
Aaron Merriam <amerriam@fusionbox.com>
2013-07-14 20:12:35 -0600

    Add email verify view

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns(
     'accounts.views',
     url(r'^$', 'dashboard', name='dashboard'),
     url(r'^edit/$', 'account_edit', name='account_edit'),
+    url(r'^verify-email/(?P<uidb36>\w+)/(?P<token>[-a-zA-Z0-9]+)/$', 'verify_email'),
 )
