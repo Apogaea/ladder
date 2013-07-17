5b2dd52a945106e9a017f90f796017fb66e184f8
Aaron Merriam <amerriam@fusionbox.com>
2013-07-16 22:52:09 -0600

    fix heroku settings

--- ladder/settings_heroku.py
+++ ladder/settings_heroku.py
@@ -19,6 +19,12 @@ AWS_ACCESS_KEY_ID = os.environ.setdefault("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.setdefault("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = 'apo-ladder'
 
+# Django Compressor
+COMPRESS_ENABLED = True
+COMPRESS_URL = '//s3.amazonaws.com/{0}/'.format(AWS_STORAGE_BUCKET_NAME)
+#COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_STORAGE = STATICFILES_STORAGE
+
 # S3 Storage Stuff
 DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
 DEFAULT_S3_PATH = "media"
@@ -31,12 +37,6 @@ MEDIA_URL = '//s3.amazonaws.com/{0}/media/'.format(AWS_STORAGE_BUCKET_NAME)
 
 STATIC_ROOT = '/{0}/'.format(STATIC_S3_PATH)
 STATIC_URL = COMPRESS_URL
-
-# Django Compressor
-COMPRESS_ENABLED = True
-COMPRESS_URL = '//s3.amazonaws.com/{0}/'.format(AWS_STORAGE_BUCKET_NAME)
-#COMPRESS_ROOT = STATIC_ROOT
-COMPRESS_STORAGE = STATICFILES_STORAGE
 
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
