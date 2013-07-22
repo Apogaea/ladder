ce6c09501bf94dd02711c649f830f067a82f5152
Aaron Merriam <amerriam@fusionbox.com>
2013-07-22 11:43:17 -0600

    fix twilio settings in travis settings

--- ladder/settings_travis.py
+++ ladder/settings_travis.py
@@ -2,7 +2,7 @@ from settings_base import *  # NOQA
 
 DEBUG = False
 
-# Twilio credentials
-TWILIO_ACCOUNT_SID = os.environ["TWILIO_ACCOUNT_SID"]
-TWILIO_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
+# Twilio testing credentials
+TWILIO_TEST_ACCOUNT_SID = os.environ["TWILIO_ACCOUNT_SID"]
+TWILIO_TEST_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
 TWILIO_PHONE_NUMBER = '+12404282876'
