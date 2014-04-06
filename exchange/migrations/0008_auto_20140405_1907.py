bc1a88b2b7235f4a943164a8a2274445db9ac02e
Piper Merriam <piper@simpleenergy.com>
2014-04-05 21:10:01 -0400

    remove users with bad phone numbers

---
+++ exchange/migrations/0008_auto_20140405_1907.py
@@ -0,0 +1,22 @@
+# encoding: utf8
+from django.db import models, migrations
+
+
+def delete_users_with_bad_phone_numbers(apps, schema_editor):
+    # We can't import the Person model directly as it may be a newer
+    # version than this migration expects. We use the historical version.
+
+    User = apps.get_model("accounts", "User")
+    User.objects.filter(_profile__isnull=True).delete()
+    User.objects.filter(_profile__phone_number='').delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('exchange', '0007_auto_20140403_2025'),
+    ]
+
+    operations = [
+        migrations.RunPython(delete_users_with_bad_phone_numbers),
+    ]
