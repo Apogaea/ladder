5a116ba934c9b9102f17fa4fa90efaba2f7b2e49
Piper Merriam <piper@simpleenergy.com>
2015-02-24 16:44:31 -0700

    fix borked factory

--- tests/accounts/factories.py
+++ tests/accounts/factories.py
@@ -1,10 +1,8 @@
 from django.utils import timezone
-from django.db.models.signals import post_save
 
 import factory
 
 from ladder.apps.accounts.models import User
-from ladder.apps.exchange.receivers import create_ladder_profile
 
 
 class UserFactory(factory.DjangoModelFactory):
@@ -15,16 +13,6 @@ class UserFactory(factory.DjangoModelFactory):
     password = factory.PostGenerationMethodCall('set_password', 'secret')
 
     last_login = factory.LazyAttribute(lambda u: timezone.now())
-
-    @classmethod
-    def _generate(cls, create, attrs):
-        """Override the default _generate() to disable the post-save signal."""
-
-        # Note: If the signal was defined with a dispatch_uid, include that in both calls.
-        post_save.disconnect(create_ladder_profile, User)
-        user = super(UserFactory, cls)._generate(create, attrs)
-        post_save.connect(create_ladder_profile, User)
-        return user
 
     class Meta:
         model = User
