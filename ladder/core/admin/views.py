1e850ec968b613538e908ea04e1cde1a9e7167d6
Piper Merriam <piper@simpleenergy.com>
2015-02-24 21:10:41 -0700

    Convert admin to use partial templates for tables

--- ladder/core/admin/views.py
+++ ladder/core/admin/views.py
@@ -8,15 +8,36 @@ from authtools.views import LoginView
 
 from ladder.core.decorators import AdminRequiredMixin
 
+from ladder.apps.exchange.models import (
+    TicketRequest,
+    TicketOffer,
+    TicketMatch,
+)
+
 User = get_user_model()
 
 
 class AdminIndexView(AdminRequiredMixin, TemplateView):
     template_name = 'admin/index.html'
 
+    def get_recent_users(self):
+        return User.objects.order_by('-date_joined')[:10]
+
+    def get_recent_requests(self):
+        return TicketRequest.objects.order_by('-created_at')[:10]
+
+    def get_recent_offers(self):
+        return TicketOffer.objects.order_by('-created_at')[:10]
+
+    def get_recent_matches(self):
+        return TicketMatch.objects.order_by('-created_at')[:10]
+
     def get_context_data(self, **kwargs):
         kwargs = super(AdminIndexView, self).get_context_data()
-        kwargs['recently_registered_users'] = User.objects.order_by('-date_joined')[:10]
+        kwargs['recently_registered_users'] = self.get_recent_users()
+        kwargs['recently_requests'] = self.get_recent_requests()
+        kwargs['recently_offers'] = self.get_recent_offers()
+        kwargs['recently_matches'] = self.get_recent_matches()
         return kwargs
 
 
