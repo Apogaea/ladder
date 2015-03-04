de459b1bfd28406ccbaf1fb3d6b79e6323c82a9d
Piper Merriam <piper@simpleenergy.com>
2015-03-03 17:26:51 -0700

    make site_id configurable

--- ladder/settings.py
+++ ladder/settings.py
@@ -45,7 +45,7 @@ TIME_ZONE = 'America/Denver'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
+SITE_ID = int(excavator.env_string('DJANGO_SITE_ID', default=1))
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
