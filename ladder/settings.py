333ebb4ae586ac64c8419a4b90d22786c5669bf7
Piper Merriam <piper@simpleenergy.com>
2015-04-27 13:02:02 -0600

    Extend to May 30th

--- ladder/settings.py
+++ ladder/settings.py
@@ -184,7 +184,7 @@ REGISTRATION_OPEN_DATE = datetime.datetime(
     year=2015, month=3, day=21, hour=13, tzinfo=pytz.timezone(TIME_ZONE),
 )
 REGISTRATION_CLOSE_DATE = datetime.datetime(
-    year=2015, month=5, day=1, hour=0, tzinfo=pytz.timezone(TIME_ZONE),
+    year=2015, month=5, day=30, hour=0, tzinfo=pytz.timezone(TIME_ZONE),
 )
 
 # Python dotted path to the WSGI application used by Django's runserver.
