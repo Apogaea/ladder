ec593b643927f423503e83cf4c0bdfdc049f1fe5
Piper Merriam <piper@simpleenergy.com>
2015-05-15 09:30:11 -0600

    Fix queries for active stuff

--- tests/exchange/test_cleanup_expired_matches_command.py
+++ tests/exchange/test_cleanup_expired_matches_command.py
@@ -1,7 +1,7 @@
 from django.core.management import call_command
 
 
-def test_match_cleanup_command(factories, models):
+def test_match_cleanup_for_fresh_match_command(factories, models):
     offer = factories.TicketOfferFactory()
     request = factories.TicketRequestFactory()
 
@@ -10,3 +10,18 @@ def test_match_cleanup_command(factories, models):
     call_command('handle_expired_matches')
 
     assert models.TicketMatch.objects.exists()
+
+
+def test_match_cleanup_for_expired_match_command(factories, models):
+    expired_match = factories.ExpiredTicketMatchFactory()
+    offer = expired_match.ticket_offer
+
+    pending_request = factories.TicketRequestFactory()
+
+    assert pending_request.is_active
+    assert offer.is_active
+
+    call_command('handle_expired_matches')
+
+    assert offer.is_reserved
+    assert pending_request.is_reserved
